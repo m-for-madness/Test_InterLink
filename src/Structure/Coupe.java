40e9a628905721765082f5da02c18eedb865f37f
m-for-madness <markiyan.yuskevych>
2017-01-05 04:41:47 +0200

    Added two extended classes, added new methods for polymorphism

---
+++ src/Structure/Coupe.java
@@ -0,0 +1,51 @@
+package Structure;
+// this class have specified fields, method sentinel for polymorphism 
+// and method characterized which uses our specified fields
+public class Coupe extends Car {
+	private double volume;
+	private int quantity_of_doors;
+	private int motor_power;
+
+	public Coupe() {
+	}
+
+	public Coupe(String model, int day_production, double volume, int q_of_d, int m_p) throws InvalidDataException {
+		super(model, day_production);
+		this.volume = volume;
+		this.quantity_of_doors = q_of_d;
+		this.motor_power = m_p;
+	}
+
+	public void sentinel() {
+		System.out.println("Output - " + this.getClass().getSimpleName());
+	}
+
+	public void charaterized() {
+		System.out.println("Power of this car = " + (volume * motor_power));
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+	public void setVolume(double volume) {
+		this.volume = volume;
+	}
+
+	public int getQuantity_of_doors() {
+		return quantity_of_doors;
+	}
+
+	public void setQuantity_of_doors(int quantity_of_doors) {
+		this.quantity_of_doors = quantity_of_doors;
+	}
+
+	public int getMotor_power() {
+		return motor_power;
+	}
+
+	public void setMotor_power(int motor_power) {
+		this.motor_power = motor_power;
+	}
+
+}
---
+++ src/Structure/Truck.java
@@ -0,0 +1,36 @@
+package Structure;
+
+public class Truck extends Autobus {
+	// the second specified field in extended class
+	private int tonnage;
+
+	public Truck(String model, int day_production, int tonnage) throws InvalidDataException, InvalidQuantityException {
+		super(model, day_production);
+		this.tonnage = tonnage;
+	}
+
+	// specified method for this class
+	public void incTonnage(int i) {
+		tonnage += i;
+	}
+
+	// it is our method for polymorphism
+	public void sentinel() {
+		System.out.println("Output the result of Truck's method - " + this.getClass().getSimpleName());
+	}
+
+	public int getTonnage() {
+		return tonnage;
+	}
+
+	public void setTonnage(int tonnage) {
+		this.tonnage = tonnage;
+	}
+
+	@Override
+	public String toString() {
+		return "Truck[model=" + model + ", day_production=" + day_production + ", tonnage=" + tonnage
+				+ ", passenger's quantity =" + super.getPassengers_quantity() + "]";
+	}
+
+}
